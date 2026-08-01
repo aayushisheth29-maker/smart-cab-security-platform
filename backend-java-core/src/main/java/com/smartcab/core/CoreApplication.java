022a9c84063f8457daf90d277510125fbd561b6b
aayushisheth29-maker <aayushicloudstudy@gmail.com>
2026-08-02 00:49:34 +0530

    Set up Java Spring Boot Enterprise Core Backend!

---
+++ backend-java-core/src/main/java/com/smartcab/core/CoreApplication.java
@@ -0,0 +1,20 @@
+package com.smartcab.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Welcome to the SmartCab Java Enterprise Backend! ☕🚀";
+	}
+}
